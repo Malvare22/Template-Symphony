d1dea63c3aecbd164a413626530cbbe4835540ca
Fireploy <fireployvm@gmail.com>
2025-05-14 16:07:42 -0500

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
 public function index(): Response
 {
-    return $this->render('home/base.html.twig',, [
+    return $this->render('home/base.html.twig', [
             'app_name' => $_ENV['APP_NAME'],
         ]);
 }
